a546e6391bb098483d15b1a228430deb59334cb6
Victoria <30833594+VittoriaLife@users.noreply.github.com>
2019-01-16 01:09:26 +0300

    Add files via upload

---
+++ test/gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+});
+
+gulp.task("imagemin", function () {
+  return gulp.src("source/img/**/*.{svg}")
+   .pipe(imagemin ([
+     imagemin.svgo()
+   ]))
+   .pipe(gulp.dest("source/img"));
+})
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
+  gulp.watch("source/*.html", gulp.series("refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series("css"));
+gulp.task("start", gulp.series("build", "server"));
